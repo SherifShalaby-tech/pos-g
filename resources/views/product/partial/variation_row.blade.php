784ee6651b9804292411de35132dce29235efe6b
doaabakhiet <doaabakhiet11@gmail.com>
2023-04-25 05:16:26 -0700

    edit

--- resources/views/product/partial/variation_row.blade.php
+++ resources/views/product/partial/variation_row.blade.php
@@ -56,15 +56,16 @@
         =>
         'form-control selectpicker v_unit', 'data-live-search'=>"true", 'placeholder' => '','onchange'=>"get_unit($units_js,$row_id)" , 'id'=>'select_unit_id_'.$row_id]) !!}
     </td>
-        {{-- @if(session('system_mode') != 'garments')
+        @if(session('system_mode') != 'garments')
             <td>{!! Form::number('variations['.$row_id.'][number_vs_base_unit]', $number_vs_base_unit , ['class' =>
                 'form-control
                 number_vs_base_unit', 'required','id'=>'number_vs_base_unit_'.$row_id]) !!}</td>
-        @endif --}}
-    <td class="@if(empty($is_service)) hide @endif default_purchase_price_td">{!! Form::text('variations['.$row_id.'][default_purchase_price]', $product_purchase_price , ['class' =>
+        @endif
+        {{-- @if(empty($is_service)) hide @endif --}}
+    <td class="default_purchase_price_td">{!! Form::text('variations['.$row_id.'][default_purchase_price]', $product_purchase_price , ['class' =>
         'form-control
         default_purchase_price', 'required']) !!}</td>
-    <td class="@if(empty($is_service)) hide @endif default_sell_price_td">{!! Form::text('variations['.$row_id.'][default_sell_price]', $product_sale_price,
+    <td class="default_sell_price_td">{!! Form::text('variations['.$row_id.'][default_sell_price]', $product_sale_price,
         ['class' => 'form-control default_sell_price', 'required']) !!}</td>
     <td> <button type="button" class="btn btn-danger btn-xs remove_row mt-2"><i class="dripicons-cross"></i></button>
     </td>
