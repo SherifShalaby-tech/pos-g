a01089b9b1053952562996757d5b3842d3883e7d
doaabakhiet <doaabakhiet11@gmail.com>
2023-08-14 09:10:23 -0700

    fix qty min

--- resources/views/add_stock/partials/product_row.blade.php
+++ resources/views/add_stock/partials/product_row.blade.php
@@ -39,7 +39,7 @@ if($stock){
     </td>
     <td>
 
-        <input type="text" class="form-control quantity quantity_{{$i}}" data-val="0" name="add_stock_lines[{{$i}}][quantity]" required min="1"
+        <input type="text" class="form-control quantity quantity_{{$i}}" data-val="0" name="add_stock_lines[{{$i}}][quantity]" required min="0.1"
             value="0"  index_id="{{$i}}">
 
     </td>
