814ee79c8114b4dd7f417a314b8af5d306662603
Raneem-ayman <73843554+Raneem-ayman@users.noreply.github.com>
2023-11-08 16:57:56 +0200

    fix

--- app/Http/Controllers/SellPosController.php
+++ app/Http/Controllers/SellPosController.php
@@ -1794,11 +1794,11 @@ class SellPosController extends Controller
     public function changeSellingPrice($variation_id){
         try {
 
-            $stockLines=AddStockLine::where('sell_price','>',0)->where('variation_id',$variation_id)
+            $stockLines=AddStockLine::where('variation_id',$variation_id)
             ->get();
-            if(!empty($stockLines)){
+            if(count($stockLines) > 0){
                 $updateData = ['sell_price'=>request()->sell_price];
-                AddStockLine::where('sell_price','>',0)->where('variation_id',$variation_id)->update($updateData);
+                AddStockLine::where('variation_id',$variation_id)->update($updateData);
                 // $stockLines->sell_price =request()->sell_price;
                 // $stockLines->save();
             }else{
