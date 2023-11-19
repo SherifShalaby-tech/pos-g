97d47ecf13ec64eb31dad32cca3b7f4c1f0b0915
doaabakhiet <doaabakhiet11@gmail.com>
2023-11-18 23:03:14 -0800

    fix price at os page

--- resources/views/sale_pos/partials/filtered_products.blade.php
+++ resources/views/sale_pos/partials/filtered_products.blade.php
@@ -1,6 +1,16 @@
 @forelse ($products->chunk(5) as $chunk)
     <tr style="font-size: 11px; padding: 5px;">
         @foreach ($chunk as $product)
+        @php
+            $Variation=\App\Models\Variation::where('id',$product->variation_id)->first();
+                if($Variation){
+                    $stockLines=\App\Models\AddStockLine::where('sell_price','>',0)->where('variation_id',$product->variation_id)->whereColumn('quantity',">",'quantity_sold')
+                    ->latest()->first();
+                    $default_sell_price=$stockLines?$stockLines->sell_price : $Variation->default_sell_price;
+
+                }
+
+        @endphp
             <td style="padding-top: 0px; padding-bottom: 0px;" class="product-img sound-btn filter_product_add"
                 data-is_service="{{ $product->is_service }}"
                 data-qty_available="{{ $product->qty_available - $product->block_qty }}"
@@ -14,11 +24,11 @@
                     <br> <span style="color: black; font-weight: bold;">{{ $product->sub_sku }}</span> <br>
                     @if (!empty($currency))
                         <span
-                            style="font-size:12px !important; font-weight: bold; color: black;">{{ @num_format($product->sell_price / $exchange_rate) . ' ' . $currency->symbol }}
+                            style="font-size:12px !important; font-weight: bold; color: black;">{{ $default_sell_price. ' ' . $currency->symbol }}
                         </span>
                     @else
                         <span
-                            style="font-size:12px !important; font-weight: bold; color: black;">{{ @num_format($product->sell_price / $exchange_rate) . ' ' . session('currency.symbol') }}
+                            style="font-size:12px !important; font-weight: bold; color: black;">{{ $default_sell_price. ' ' . session('currency.symbol') }}
                         </span>
                     @endif
                 </p>
